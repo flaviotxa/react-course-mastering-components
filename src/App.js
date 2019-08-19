454509fafffe172cd25495444409fc3f1a2757d0
Flavio Teixeira Lopes <flaviotxa@gmail.com>
2019-08-18 22:31:47 -0300

    Create state for the app component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import VideoList from './components/VideoList';
 import VideoPlayer from './components/VideoPlayer';
+import VideoCinema from './components/VideoCinema'
 
-function App() {
-  return (
-    <div className="App">
-    <VideoPlayer />
-    <VideoList />
-    </div>
-  );
+class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: {
+        img: 'https://static.videezy.com/system/resources/thumbnails/000/021/578/small/drone-flies-away-beach-4K.jpg',
+        name: 'Away-Beach',
+        url: 'https://static.videezy.com/system/resources/previews/000/021/578/original/drone-flies-away-beach-4K.mp4'
+      }
+    }
+  }
+
+  render() {
+    const { state } = this;
+    return (
+      <div className="App">
+        <VideoPlayer video={ state.selectedVideo } />
+        <VideoList video={ state.videos } />
+        <VideoCinema isActive={ false }/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -2,10 +2,11 @@ import React, {Component} from 'react';
 
 class VideoPlayer extends Component{
     render(){
+        const { video } = this.props
         return (
             <div className="video-player" >
                 <video 
-                    src={'https://storage.coverr.co/videos/Gubat%20Beach?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjExNDMyN0NEOTRCMUFCMTFERTE3IiwiaWF0IjoxNTY1NjYyODYwLCJleHAiOjE1NjU2NjY0NjB9.T-5NcNNWEqJYb1psBsiwFLAlZNN9h_BIUSzSWZ0tMj4'}
+                    src={ video.url }
                     controls autoPlay loop
                 />
                 <button>[ ]</button>
