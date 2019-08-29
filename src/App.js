e4932fd444351104a9f7494f7e56b7d5bb67daa9
Flavio Teixeira Lopes <flaviotxa@gmail.com>
2019-08-29 01:33:25 -0300

    Create a react portal to change the video container

--- src/App.js
+++ src/App.js
@@ -15,24 +15,37 @@ class App extends Component {
     super(props);
 
     this.selectVideo = this.selectVideo.bind(this);
+    this.toggleCinema = this.toggleCinema.bind(this);
 
     this.inlineVideo = React.createRef();
     this.cinemaVideo = React.createRef();
 
     this.state = {
       videos: [],
-      selectedVideo: {}
+      selectedVideo: {},
+      videoContainerElement: this.inlineVideo
     }
   }
 
   async componentDidMount() {
     const videos = await VideoService.list();
     this.setState({videos});
-    Channel.on('video:select', this.selectVideo)
+    Channel.on('video:select', this.selectVideo);
+    Channel.on('video:toggle-cinema', this.toggleCinema);
   }
 
   componentWillUnmount() {
     Channel.removeListener('video:select', this.selectVideo);
+    Channel.removeListener('video:toggle-cinema', this.toggleCinema);
+  }
+
+  toggleCinema() {
+    const currentElement = this.state.videoContainerElement,
+      newContainer = currentElement === this.inlineVideo ? this.cinemaVideo : this.inlineVideo;
+    
+    this.setState({
+      videoContainerElement : newContainer
+    });
   }
 
   selectVideo(video) {
@@ -44,12 +57,12 @@ class App extends Component {
 
     return (
       <div className="App">
-        <VideoPlayer video={ state.selectedVideo } />
+        <VideoPlayer video={ state.selectedVideo } container={state.videoContainerElement.current}/>
         <VideoInline>
           <div ref={this.inlineVideo} ></div>
         </VideoInline>
         <VideoList videos={ state.videos } />
-        <VideoCinema isActive={ false }>
+        <VideoCinema isActive={ state.videoContainerElement === this.cinemaVideo }>
           <div ref={this.cinemaVideo} ></div>
         </VideoCinema>
       </div>
